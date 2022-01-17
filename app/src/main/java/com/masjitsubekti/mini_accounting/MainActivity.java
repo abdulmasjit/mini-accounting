04323a0d141250b47c1cbdd5a799f8c6607c6d3a
bariq-qs <bariq.xtkj1.14@gmail.com>
2022-01-17 22:49:37 +0700

    halaman home page belum selesai

--- app/src/main/java/com/masjitsubekti/mini_accounting/MainActivity.java
+++ app/src/main/java/com/masjitsubekti/mini_accounting/MainActivity.java
@@ -2,13 +2,30 @@ package com.masjitsubekti.mini_accounting;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    Button cirLoginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        cirLoginButton = findViewById(R.id.cirLoginButton);
+        cirLoginButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.cirLoginButton:
+                Intent intent = new Intent(this, ProfileActivity.class);
+                startActivity(intent);
+                break;
+        }
     }
 }

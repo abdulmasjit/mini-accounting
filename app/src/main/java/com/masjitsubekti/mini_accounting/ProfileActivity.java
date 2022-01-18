9fa15baca8f15349dac9fbf5895972a8aa6a4450
bariq-qs <bariq.xtkj1.14@gmail.com>
2022-01-19 00:33:29 +0700

    added Simpanan page

--- app/src/main/java/com/masjitsubekti/mini_accounting/ProfileActivity.java
+++ app/src/main/java/com/masjitsubekti/mini_accounting/ProfileActivity.java
@@ -2,13 +2,30 @@ package com.masjitsubekti.mini_accounting;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
-public class ProfileActivity extends AppCompatActivity {
+public class ProfileActivity extends AppCompatActivity implements View.OnClickListener{
 
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        imageView = findViewById(R.id.imageView);
+        imageView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.imageView:
+                Intent intent = new Intent(this, Simpanan.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
